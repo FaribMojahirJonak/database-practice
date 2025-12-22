4e2afa32599bf04f8e8521e9cdc9852a586004a4
FaribMojahirJonak <fmjnk248@gmail.com>
2025-12-22 15:33:46 +0600

    database practice

---
+++ database-practice/src/student/student.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './student.schema';
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'Student',
+                schema: StudentSchema
+            }
+        ])
+    ],
+})
+export class StudentModule {}
---
+++ database-practice/src/student/student.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { timestamp } from "rxjs";
+
+export type StudentDocument = Student & Document;
+@Schema( { timestamps: true } )
+export class Student {
+    @Prop({ required: true })
+    name: string;
+    @Prop({ required: true })
+    age: number;
+    @Prop({ required: true })
+    grade: string;
+    @Prop()
+    email? : string;
+}
+export const StudentSchema = SchemaFactory.createForClass(Student);
