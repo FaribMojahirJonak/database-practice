852e45c97f8c76b259095e3fa616958582cb10f5
FaribMojahirJonak <fmjnk248@gmail.com>
2025-12-23 11:01:36 +0600

    http methods with mongodb

--- database-practice/src/student/student.controller.ts
+++ database-practice/src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.schema';
 
@@ -12,4 +12,35 @@ export class StudentController {
     async createStudent(@Body() data: Partial<Student>) {
         return this.studentService.createStudent(data);
     }
-}
+
+    @Get()
+    async getAllStudent() : Promise<Student[]> {
+        return this.studentService.getAllStudents();
+    }
+
+    @Get(':id')
+    async getStudentById(@Param('id') id : string) : Promise<Student | null> {
+        return this.studentService.getStudentById(id);
+    }
+
+    @Put(':id')
+    async updateStudent(
+        @Param('id') id : string,
+        @Body() data : Partial<Student>
+    ) : Promise<Student | null> {
+        return this.studentService.updateStudent(id, data);
+    }
+
+    @Patch(':id')
+    async patchStudent(
+        @Param('id') id : string,
+        @Body() data : Partial<Student>
+    ) : Promise<Student | null> {
+        return this.studentService.patchStudent(id, data);
+    }
+
+    @Delete(':id')
+    async deleteStudent(@Param('id') id : string) : Promise<Student | null> {
+        return this.studentService.deleteStudent(id);
+    }
+} 
